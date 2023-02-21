f70421b367454750d109154c9e3ea9e65f30c5ab
Dmitry Kozhevnikov <kda1980rus@yandex.ru>
2023-02-21 11:33:32 +0400

    edits after the review

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 // Получаем элемент редактирования иконки
 const editProfile = document.querySelector('.profile__editor');
 // Получаем popup редактирования профиля
-const popupEditProfile = document.querySelector('.popup-edit');
+const popupEditProfile = document.querySelector('.popup');
 // Получаем элемент иконки закрытия popup
-const popupCloseIcon = popupEditProfile.querySelector('.popup-edit__close');
+const popupCloseIcon = popupEditProfile.querySelector('.popup__close');
 // Получаем имя профиля
 const profileName = document.querySelector('.profile__name');
 // Получаем описание профиля
 const profileDescription = document.querySelector('.profile__description');
 // Получаем input имени
-const nameInput = popupEditProfile.querySelector('.popup-edit__input_item_name');
+const nameInput = popupEditProfile.querySelector('.popup__input_name');
 // Получаем input описания
-const descriptionInput = popupEditProfile.querySelector('.popup-edit__input_item_description');
+const descriptionInput = popupEditProfile.querySelector('.popup__input_description');
 
 // Функция открытия popup
 const popupOpen = function () {
-  popupEditProfile.classList.add('popup-edit_opened');
+  popupEditProfile.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   descriptionInput.value = profileDescription.textContent;
 }
 // Функция закрытия popup
 const popupClose = function () {
-  popupEditProfile.classList.remove('popup-edit_opened');
+  popupEditProfile.classList.remove('popup_opened');
 }
 // Функция сохранения введённых в форму данных (имени и описания)
 const formSubmitHandler = function (evt) {
